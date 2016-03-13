0758af659778dcc37b5b65c71ac21c7cfa94f1f1
Juan Carlos <juancoz@hotmail.com>
2016-03-13 14:37:39 -0500

    Hook the adapter up to the ListView

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,6 +26,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         String [] forecastArray = {
                 "Today - Sunny - 88/63",
                 "Tomorrow - Foggy - 70/46",
@@ -40,6 +43,9 @@ public class MainActivityFragment extends Fragment {
                 R.id.list_item_forecast_textview,
                 weekForecast);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
