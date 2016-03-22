6153fe60f054c1eca7200e3a0a0b355e6b4b6ac1
Juan Carlos <juancoz@hotmail.com>
2016-03-22 15:09:35 -0500

    Write the urimatcher

--- app/src/androidTest/java/com/example/android/sunshine/app/TestFetchWeatherTask.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/TestFetchWeatherTask.java
@@ -17,7 +17,7 @@ package com.example.android.sunshine.app;
 
 import android.test.AndroidTestCase;
 
-public class TestFetchWeatherTask extends AndroidTestCase{
+public class TestFetchWeatherTask extends AndroidTestCase {
     static final String ADD_LOCATION_SETTING = "Sunnydale, CA";
     static final String ADD_LOCATION_CITY = "Sunnydale";
     static final double ADD_LOCATION_LAT = 34.425833;
